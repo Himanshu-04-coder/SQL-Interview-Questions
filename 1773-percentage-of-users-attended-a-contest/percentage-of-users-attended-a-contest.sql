e743c58adbdad2dc1520cdf0d5f1ddbfc54ecb85
Himanshu Singh <himanshusingh8102004@gmail.com>
2026-07-13 22:37:07 +0530

    Time: 978 ms (64.09%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -12,3 +12,16 @@ LEFT JOIN
 ON r.user_id = u.user_id
 GROUP BY contest_id
 ORDER BY percentage DESC , contest_id;
+
+-- LEAST COMPLEX
+
+-- SELECT r.contest_id,
+--     ROUND(
+--         COUNT(DISTINCT r.user_id) * 100.0 /
+--         (SELECT COUNT(*) FROM Users), 
+--         2
+--     ) AS percentage
+
+-- FROM Register r
+-- GROUP BY r.contest_id
+-- ORDER BY percentage DESC, r.contest_id;
